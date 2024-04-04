66a7486cb2d31298e7b40c7f8ffc3b14159ad20b
Luca <luca@perugia.ovh>
2024-04-04 20:57:16 +0200

    Fix ms logger spam :)

--- ShockOsc/MauiProgram.cs
+++ ShockOsc/MauiProgram.cs
@@ -22,31 +22,29 @@ public static class MauiProgram
         builder.Services.AddBlazorWebViewDeveloperTools();
         builder.Logging.AddDebug();
 #endif
-        
-        Log.Logger = new LoggerConfiguration()
+
+        var loggerConfiguration = new LoggerConfiguration()
             .MinimumLevel.Information()
             .Filter.ByExcluding(ev =>
-                ev.Exception is InvalidDataException a && a.Message.StartsWith("Invocation provides")).Filter.ByExcluding(x => x.MessageTemplate.Text.StartsWith("Failed to find handler for"))
+                ev.Exception is InvalidDataException a && a.Message.StartsWith("Invocation provides")).Filter
+            .ByExcluding(x => x.MessageTemplate.Text.StartsWith("Failed to find handler for"))
+            .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
             .WriteTo.UiLogSink()
-            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}")
-            .CreateLogger();
+            .WriteTo.Console(
+                outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}");
 
         // ReSharper disable once RedundantAssignment
-        var isDebug = false;
+        var isDebug = Environment.GetCommandLineArgs().Any(x => x.Equals("--debug", StringComparison.InvariantCultureIgnoreCase));
+        
 #if DEBUG
         isDebug = true;
 #endif
         if (isDebug)
         {
-            Log.Information("Debug logging enabled");
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .Filter.ByExcluding(ev =>
-                    ev.Exception is InvalidDataException a && a.Message.StartsWith("Invocation provides")).Filter.ByExcluding(x => x.MessageTemplate.Text.StartsWith("Failed to find handler for"))
-                .WriteTo.UiLogSink()
-                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}")
-                .CreateLogger();
+            loggerConfiguration.MinimumLevel.Debug();
         }
+
+        Log.Logger = loggerConfiguration.CreateLogger();
         
         builder.Services.AddSerilog(Log.Logger);
 
