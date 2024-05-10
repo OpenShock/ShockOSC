e1d377e05de8f9b85656f18ddef071b92c95fe45
Luca <luca@perugia.ovh>
2024-05-11 00:40:20 +0200

    Disable discord preview by default

--- ShockOsc/Config/AppConfig.cs
+++ ShockOsc/Config/AppConfig.cs
@@ -5,7 +5,7 @@ namespace OpenShock.ShockOsc.Config;
 public sealed class AppConfig
 {
     public bool CloseToTray { get; set; } = true;
-    public bool DiscordPreview { get; set; } = true;
+    public bool DiscordPreview { get; set; } = false;
     
     public UpdateChannel UpdateChannel { get; set; } = UpdateChannel.Release;
     public SemVersion? LastIgnoredVersion { get; set; } = null;
