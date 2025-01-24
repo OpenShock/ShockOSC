944f8f695d43afd4d57e393a97a50bbef18081d3
Luc <luc@luc.cat>
2025-01-24 20:51:37 +0100

    Add module def

---
+++ ShockOsc/ShockOSCModule.cs
@@ -0,0 +1,16 @@
+﻿using ModuleBase;
+
+namespace OpenShock.ShockOsc;
+
+public sealed class ShockOSCModule : IModule
+{
+    public string Id => "OpenShock.ShockOsc";
+    public string Name => "ShockOSC";
+    public Type RootComponentType { get; }
+    public string IconPath => "OpenShock.ShockOsc.Resources.ShockOSC-Icon.png";
+    
+    public void Start()
+    {
+        
+    }
+}
